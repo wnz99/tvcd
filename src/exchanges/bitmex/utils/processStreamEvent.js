73bbe8a6dcbdd6c839732d27774f512cbe137c64
David Fava <dfava80@gmail.com>
2020-05-17 10:24:14 +0100

    Fix bitmex ws symbol processing

--- src/exchanges/bitmex/utils/processStreamEvent.js
+++ src/exchanges/bitmex/utils/processStreamEvent.js
@@ -4,10 +4,9 @@ const processStreamEvent = (event) => {
     const { data, table } = msg;
     const interval = table.replace('tradeBin', '');
     const { timestamp, open, high, low, close, volume, symbol } = data[0];
-    const ticker = `${symbol.slice(0, -3)}USD`;
     const candleData = { timestamp, open, close, high, low, volume };
 
-    return [ticker, candleData, interval];
+    return [symbol, candleData, interval];
   } catch (e) {
     // eslint-disable-next-line no-console
     console.warn(e);
