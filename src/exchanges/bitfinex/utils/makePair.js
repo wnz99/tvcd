b5dd2b05fcbbad88bdd6bd59c129a7cf2b208dad
David Fava <dfava80@gmail.com>
2021-01-17 10:35:11 +0000

    Fix import

--- src/exchanges/bitfinex/utils/makePair.js
+++ src/exchanges/bitfinex/utils/makePair.js
@@ -1,5 +1,3 @@
-import { PAIRS_MAP } from '../const';
-
 const makePair = (baseSymbol, quoteSymbol) => {
   if (baseSymbol.length === 3 && quoteSymbol.length === 3) {
     return `${baseSymbol}${quoteSymbol}`;
